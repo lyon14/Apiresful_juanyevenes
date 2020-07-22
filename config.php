52b68dbb1f6afcbc67f479dc6beb0a013a9c53d8
juan yévenes <34106441+lyon14@users.noreply.github.com>
2020-07-22 17:27:51 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 $db = [
-    'host' => 'MYSQLCONNSTR_localdb',
+    'host' => '9e27a33b-3769-418f-9d78-02b6dea3b7a7',
     'username' => 'juan1426',
     'password' => 'rosita1426',
     'db' => 'posts' //Cambiar al nombre de tu base de datos
