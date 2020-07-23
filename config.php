3d0834ee53c5b2ca89120d805693082fa7e1e64a
juan yévenes <34106441+lyon14@users.noreply.github.com>
2020-07-23 17:28:19 -0400

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $db = [
     'host' => 'posts.mysql.database.azure.com',
     'username' => 'juan1426',
-    'password' => 'rosita1426',
+    'password' => 'Rosita1426',
     'db' => 'posts' //Cambiar al nombre de tu base de datos azure@localhost 
 ];
 
